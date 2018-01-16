2028064cdd6c7b3436e3a622330e968e2b5c2faa
Keshav885 <kesava.paruchuri@aurotechcorp.com>
2018-01-15 19:08:55 -0500

    dicey launched

---
+++ Diceyy/Diceyy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Diceyy
+//
+//  Created by Keshav Paruchuri on 1/15/18.
+//  Copyright © 2018 Keshav Paruchuri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
